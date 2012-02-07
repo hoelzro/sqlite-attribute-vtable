e50cf83934fe96669cd9349166bcabc0e7e3bf4b
Rob Hoelz <rhoelz@inoc.com>
2012-02-07 17:24:35 -0600

    Refactor test a bit

--- t/00-loadable.t
+++ t/00-loadable.t
@@ -9,15 +9,13 @@ check_deps;
 
 plan tests => 1;
 
-require DBI;
+my $dbh = create_dbh(no_load => 1);
 
-my $dbh = DBI->connect('dbi:SQLite:dbname=:memory:', undef, undef, {
-    PrintError => 0,
-});
+my $ok = do {
+    local $dbh->{'RaiseError'} = 0;
 
-$dbh->sqlite_enable_load_extension(1);
-
-my $ok = $dbh->do(q{SELECT load_extension('./attributes.so', 'sql_attr_init')});
+    $dbh->do(q{SELECT load_extension('./attributes.so', 'sql_attr_init')});
+};
 
 ok($ok, 'attribute extension should successfully load') or diag($dbh->errstr);
 
--- t/lib/SQLite/TestUtils.pm
+++ t/lib/SQLite/TestUtils.pm
@@ -6,7 +6,7 @@ use parent 'Exporter';
 
 use Test::More;
 
-our @EXPORT = qw(check_deps);
+our @EXPORT = qw(check_deps create_dbh);
 
 sub check_deps {
     my $ok = eval {
@@ -18,6 +18,25 @@ sub check_deps {
         plan skip_all => q{The DBD::SQLite module is required to run this test};
         exit 0;
     }
+}
+
+sub create_dbh {
+    my ( %options ) = @_;
+
+    require DBI;
+
+    my $dbh = DBI->connect('dbi:SQLite:dbname=:memory:', undef, undef, {
+        RaiseError => 1,
+        PrintError => 0,
+    });
+
+    $dbh->sqlite_enable_load_extension(1);
+
+    unless($options{'no_load'}) {
+        $dbh->do(q{SELECT load_extension('./attributes.so', 'sql_attr_init')});
+    }
+
+    return $dbh;
 }
 
 1;
